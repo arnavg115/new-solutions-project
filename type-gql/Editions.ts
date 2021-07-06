b706a91d02b2d1de6ba560841f65d8fd5cc2f2d9
Arnav G <pyeushgurha@pyeushs-imac.attlocal.net>
2021-07-05 22:57:57 -0700

    fixed bug

--- type-gql/Editions.ts
+++ type-gql/Editions.ts
@@ -5,11 +5,14 @@ import { Article } from "./Article";
 @ObjectType()
 export class Editions {
   @Field(() => Int)
+  //   @ts-ignore
   num: number;
 
   @Field()
+  //   @ts-ignore
   name: string;
 
   @Field(() => [Article])
+  //   @ts-ignore
   articles: Article[];
 }
